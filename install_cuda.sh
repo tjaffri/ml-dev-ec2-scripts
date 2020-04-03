402d280f3a4b0e35b2a24ae012c2eeb56ef0641d
Taqi Jaffri <tjaffri@gmail.com>
2020-04-03 15:17:37 -0700

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,4 +1,4 @@
-CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
 wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG}
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo dpkg -i /tmp/${CUDA_REPO_PKG}
