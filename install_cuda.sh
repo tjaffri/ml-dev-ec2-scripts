6253627d8db310ae2ebdc8a9414edab6b006beb2
Taqi Jaffri <tjaffri@gmail.com>
2020-04-03 18:23:44 -0700

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,9 +1,12 @@
+set -e
+
 CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
 wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG}
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo dpkg -i /tmp/${CUDA_REPO_PKG}
 
 sudo apt-get update --yes
+sudo apt-get upgraade --yes
 sudo apt-get install cuda-drivers --yes
 sudo apt-get install cuda --yes
 
@@ -14,21 +17,27 @@ docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a 
 sudo apt-get purge -y nvidia-docker
 
 # Add the package repositories
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-  sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# install docker
 sudo apt-get update --yes
+sudo apt-get install docker-ce
+sudo usermod -aG docker ubuntu
+su - ubuntu
 
 # Install nvidia-docker2 and reload the Docker daemon configuration
-DOCKER_CE_VERSION=`apt-cache show nvidia-docker2 | grep -Po "(?<=docker-ce \(= )([^)]*)" | head -n 1`
-sudo apt-get -yq --allow-downgrades install nvidia-docker2 docker-ce=${DOCKER_CE_VERSION}
-
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+ sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+ sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
 
 # Test nvidia-smi with the latest official CUDA image
 docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
 
-
+echo "**** if the above NVIDIA-SMI output shows your GPU, you are good to go!"
 echo "****** restart this VM"
