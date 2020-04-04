c9bd2ed45b98c5ece5ed8f8a215ef17a592904f5
Taqi Jaffri <tjaffri@gmail.com>
2020-04-03 20:02:45 -0700

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -45,7 +45,7 @@ sudo apt-get install -y nodejs
 ############ agent extract and setup
 cd ~
 wget https://vstsagentpackage.azureedge.net/agent/2.165.2/vsts-agent-linux-x64-2.165.2.tar.gz
-mkdir myagent && cd myagent
+mkdir -p myagent && cd myagent
 tar -xzf ../vsts-agent-linux-x64-2.165.2.tar.gz
 ./config.sh # requires input
 sudo ./svc.sh install
