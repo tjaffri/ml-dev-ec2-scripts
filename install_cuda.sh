63d7724306974c806f2e9a0b3ce069520db0636b
Taqi Jaffri <tjaffri@gmail.com>
2020-04-27 04:19:37 -0700

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,3 +1,5 @@
+set -e
+
 CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
 wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG}
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
@@ -13,7 +15,7 @@ sudo apt-get install cuda --yes
 
 # If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
 docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
-sudo apt-get purge -y nvidia-docker
+sudo apt-get purge -y nvidia-docker || true
 
 # Add the package repositories
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
